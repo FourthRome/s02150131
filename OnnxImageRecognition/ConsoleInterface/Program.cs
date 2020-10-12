573a4aaa02f5382a852f7246f6e032bfb65aa0d1
Roman Krivonogov <romansdidnotcrucify@gmail.com>
2020-10-12 11:49:58 +0300

    Initial codebase (#1)
    * Added project files
    
    * Separated projects' logic
    
    Now console interface's 'Main()' calls static 'Recognize(path)' function from image recognizer project. I also added a dummy check for exceptions in reading the image.
    
    'var' keyword is now omitted in 'Recognize', which might be a source of extensive memory consumption (depending on how GC works). TODO: find a way to fix that.
    
    * Changed logging a bit; still lacking a best practice async logging solution
    
    * Small comment refactoring
    
    * Changed project structure
    
    * Basic directory traversing
    
    * A small fix
    
    * Replaced console in class lib with message queue
    
    It's not parallel or async yet, but it's a step forward.
    
    * Ignoring .onnx files
    
    * Added parallel processing, but not printing
    
    Output is still provided after the tasks have completed their job.
    
    * Added exception handling for missing model
    
    * Added asynchronous queue reading
    
    It is still prone to errors and requires refactoring; that's the next major priority.
    
    * Added sample images
    
    Now that there are more images some problems are seen.
    
    * Fixed result printing
    
    Synchronized it with writing (using WritingPermission semaphor in console app), started printing the leftovers of the queue. Now the results are complete!
    
    * Added proper cancellation
    
    It needs additional testing, on a bigger dataset. I also added some new comments.
    
    * Added readme-s
    
    * Added more samples

---
+++ OnnxImageRecognition/ConsoleInterface/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ImageRecognizer;
+
+namespace ConsoleInterface
+{
+    class Program
+    {
+        static readonly TaskStatus[] recognitionFinishedMarkers = {
+            TaskStatus.Faulted,
+            TaskStatus.Canceled,
+            TaskStatus.RanToCompletion,
+        };
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Type the full path to the folder, and image recognition will begin:");
+            string path = Console.ReadLine();
+
+            // Create cancellation items
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken cancelToken = cts.Token;
+
+            // Run task to wait for cancellation from keyboard
+            var cancellationTask = Task.Run(() => { CancellationWaiter(cts); }, cancelToken);
+
+            // Start processing
+            var traversing = MnistRecognizer.TraverseDirectory(path, cancelToken);
+
+            // Print new items in results' queue
+            while (!recognitionFinishedMarkers.Contains(traversing.Status))
+            {
+                await MnistRecognizer.NewResults.WaitAsync();
+                await MnistRecognizer.WritePermission.WaitAsync();
+                PrintQueue();
+                MnistRecognizer.ResultsQueue.Clear();
+                MnistRecognizer.WritePermission.Release();
+                await Task.Delay(100);
+            }
+
+            PrintQueue();
+        }
+
+        static void PrintQueue()
+        {
+            foreach (var entry in MnistRecognizer.ResultsQueue)
+            {
+                Console.WriteLine($"Results for {entry.ImagePath}:");
+
+                foreach (var output in entry.ModelOutput)
+                {
+                    Console.WriteLine($"{output.Label} with confidence {output.Confidence}");
+                }
+            }
+        }
+
+        static void CancellationWaiter(CancellationTokenSource cts)
+        {
+            while (!cts.Token.IsCancellationRequested)
+            {
+                if (!Console.KeyAvailable)
+                {
+                    Task.Delay(100);
+                }
+                else
+                {
+                    var key = Console.ReadKey(intercept: true);
+                    if (key.Key == ConsoleKey.C)
+                    {
+                        cts.Cancel();
+                        Console.WriteLine("Termination is requested...");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ OnnxImageRecognition/ImageRecognizer/MnistRecognizer.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.PixelFormats;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using System.Threading;
+
+namespace ImageRecognizer
+{
+    public struct RecognitionResult
+    {
+        public struct ResultEntry
+        {
+            public string Label { get; set; }
+            public double Confidence { get; set; }
+        
+            public ResultEntry(string label, double confidence)
+            {
+                Label = label;
+                Confidence = confidence;
+            }
+        }
+
+        public string ImagePath { get; set; }
+        public List<ResultEntry> ModelOutput { get; }
+
+        // TODO: make RecognitionResult data class
+        
+        public RecognitionResult(string path, List<ResultEntry> modelOutput) 
+        {
+            ImagePath = path;
+            ModelOutput = modelOutput;
+        }
+    }
+
+
+    public class MnistRecognizer
+    {
+        public static ConcurrentQueue<RecognitionResult> ResultsQueue;
+        public static SemaphoreSlim NewResults;  // To notify about new entries
+        public static SemaphoreSlim WritePermission;  // To synchronize enqueue()
+
+        static MnistRecognizer()
+        {
+            ResultsQueue = new ConcurrentQueue<RecognitionResult>();
+            NewResults = new SemaphoreSlim(0, 1);
+            WritePermission = new SemaphoreSlim(1, 1);
+        }
+
+        public static async Task TraverseDirectory(string path, CancellationToken cancelToken = default)
+        {
+            System.IO.DirectoryInfo dir;
+            List<Task> routines = new List<Task>();
+            try
+            {
+                // TODO: traverse directory in batches
+                dir = new System.IO.DirectoryInfo(path);
+                if (dir.Exists)
+                {
+                    foreach (FileInfo fi in dir.GetFiles())
+                    {
+                        // Cancel adding new tasks, but all tasks that are started should be completed
+                        cancelToken.ThrowIfCancellationRequested();
+                        routines.Add(Task.Factory.StartNew(() =>
+                        {
+                            Recognize(fi.FullName);
+                        }, cancelToken));
+                    }
+                }
+                else
+                {
+                    System.Diagnostics.Trace.WriteLine($"[INCORRECT PATH] MnistRecognizer.TraverseDirectory: Could not open \"{path}\"; the directory might not exist.");
+                }
+            }
+            catch
+            {
+                System.Diagnostics.Trace.WriteLine($"[INCORRECT PATH] MnistRecognizer.TraverseDirectory: Could not get info about directory \"{path}\"; the directory might not exist.");
+            }
+            
+            await Task.WhenAll(routines);
+        }
+
+
+        // TODO: model path should be a parameter thorugh the whole class lib
+        public static void Recognize(string path, string modelPath="mnist-8.onnx")
+        {
+            Image<Rgb24> image = null;
+            try
+            {
+                image = Image.Load<Rgb24>(path);
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Trace.WriteLine($"[FILE ERROR] MnistRecognizer.Recognize: Could not read image \"{path}\": \n{e.Message}");
+                return;
+            }
+
+            // These come from the model's requirements
+            const int TargetWidth = 28;
+            const int TargetHeight = 28;
+
+            // Changing image size and making it grayscale
+            image.Mutate(x =>
+            {
+                x.Resize(new ResizeOptions
+                {
+                    Size = new Size(TargetWidth, TargetHeight),
+                    Mode = ResizeMode.Crop
+                });
+                x.Grayscale();
+            });
+
+            // Converting to a tensor and normalization
+            var input = new DenseTensor<float>(new[] { 1, 1, TargetHeight, TargetWidth });
+            for (int y = 0; y < TargetHeight; y++)
+            {
+                // TODO: rewrite normalization properly
+                Span<Rgb24> pixelSpan = image.GetPixelRowSpan(y);
+                for (int x = 0; x < TargetWidth; ++x)
+                {
+                    input[0, 0, y, x] = pixelSpan[x].R / 255f;
+                }
+            }
+
+            // Preparing neural net's inputs. 'Input3' name is used in the model itself
+            var inputs = new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("Input3", input)
+            };
+
+            // Inference
+            InferenceSession session;
+            try
+            {
+                session = new InferenceSession(modelPath);
+            }
+            catch  // TODO: make sure only this class of exceptions is caught here, and manage the others
+            {
+                System.Diagnostics.Trace.WriteLine($"[FILE ERROR] MnistRecognizer.Recognize: Could not find \"{modelPath}\". Please follow the README.md and retry.");
+                return;
+            }
+
+            // Getting results
+            using IDisposableReadOnlyCollection<DisposableNamedOnnxValue> results = session.Run(inputs);
+
+            // Applying softmax
+            var output = results.First().AsEnumerable<float>().ToArray();
+            var sum = output.Sum(x => (float)Math.Exp(x));
+            var softmax = output.Select(x => (float)Math.Exp(x) / sum);
+
+            var res = softmax
+                .Select((x, i) => new RecognitionResult.ResultEntry(label : i.ToString(), confidence : x))
+                .OrderByDescending(x => x.Confidence)
+                .Take(10)
+                .ToList();
+
+            // Put the new result into the queue
+            WritePermission.Wait();
+            ResultsQueue.Enqueue(new RecognitionResult(path, res));
+            try { NewResults.Release(); } catch { }  // TODO: find a better solution for mutiple-releases-should-count-as-one problem
+            WritePermission.Release();
+        }
+    }
+}
