370367f3b773b54b979bf24035508678f8c745fa
Roman Krivonogov <romansdidnotcrucify@gmail.com>
2021-02-16 20:05:21 +0300

    A commit to just get started after a long break

--- ImageRecognitionGui/GraphicalInterface/MainWindow.xaml.cs
+++ ImageRecognitionGui/GraphicalInterface/MainWindow.xaml.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ImageRecognizer;
 
 namespace GraphicalInterface
 {
@@ -20,9 +23,39 @@ namespace GraphicalInterface
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Task recognitionTask;
+
         public MainWindow()
         {
             InitializeComponent();
+            recognitionTask = null;
         }
+
+        public void OnClickStart(object sender, RoutedEventArgs e)
+        {
+            MnistRecognizer.CancelTokenSource = new CancellationTokenSource();
+            //recognitionTask = Task.Run(async () => { await MnistRecognizer.ProcessImagesInDirectory(); });
+        }
+
+        public void OnClickAbort(object sender, RoutedEventArgs e)
+        {
+            MnistRecognizer.CancelTokenSource.Cancel();
+        }
+
+        public void OnClickChooseFolder(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                  
+            }
+        }
+
+        public void RecognitionCallback()
+        {
+
+        }
+
     }
 }
