5c2a6a467024fb4145a76ebd1f55f24a633b2d86
Roman Krivonogov <romansdidnotcrucify@gmail.com>
2021-02-17 17:54:15 +0300

    Insatisfactory attempt to use library's API

--- ImageRecognitionGui/GraphicalInterface/MainWindow.xaml.cs
+++ ImageRecognitionGui/GraphicalInterface/MainWindow.xaml.cs
@@ -1,19 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Forms;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Diagnostics;
 using ImageRecognizer;
 
 namespace GraphicalInterface
@@ -23,25 +11,23 @@ namespace GraphicalInterface
     /// </summary>
     public partial class MainWindow : Window
     {
-        public Task recognitionTask;
-        public string[] imageLabels; 
-
+        private ViewModel viewModel;
         //---------------------------------------------
         // Constructors + window's basic event handlers
         //---------------------------------------------
         public MainWindow()
         {
             InitializeComponent();
-            recognitionTask = null;
-            imageLabels = MnistRecognizer.ImageLabels;
+            viewModel = new ViewModel();
+            DataContext = viewModel;
         }
 
         public void OnClickStart(object sender, RoutedEventArgs e)
         {
-            recognitionTask = Task.Run(async () => { await MnistRecognizer.ProcessImagesInDirectory(directoryPathTextBox.Text, RecognitionCallback); });
+            viewModel.RecognitionTask = Task.Run(async () => { await MnistRecognizer.ProcessImagesInDirectory(directoryPathTextBox.Text, RecognitionCallback); });
         }
 
-        public void OnClickAbort(object sender, RoutedEventArgs e)
+        public void OnClickStop(object sender, RoutedEventArgs e)
         {
             MnistRecognizer.StopProcessing();
         }
@@ -52,13 +38,14 @@ namespace GraphicalInterface
 
             if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                  
+                directoryPathTextBox.Text = dlg.SelectedPath;
             }
         }
 
         public void RecognitionCallback(RecognitionResult result)
         {
-
+            directoryPathTextBox.Text = "oo";
+            Trace.WriteLine("Hooray!");
         }
 
     }
---
+++ ImageRecognitionGui/GraphicalInterface/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ImageRecognizer;
+using System.Threading.Tasks;
+
+namespace GraphicalInterface
+{
+    class ViewModel
+    {
+        public string[] ImageLabels { get; private set;}
+
+        public Task RecognitionTask { get; set; }
+
+        public ViewModel()
+        {
+            ImageLabels = MnistRecognizer.ImageLabels;
+            RecognitionTask = null;
+        }
+    }
+}
